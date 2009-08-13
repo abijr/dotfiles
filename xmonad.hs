b85408d2ab77da85f9fe512424fc4cf72fd89d99
abijr <abijr.mtz@gmail.com>
2009-08-13 13:27:41 -0700

    edited modk-w to launch thunar

--- xmonad.hs
+++ xmonad.hs
@@ -80,6 +80,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
  
     -- launch dmenu
     , ((modm,               xK_p     ), spawn "dmenuz")
+
+	-- launch thunar
+	, ((modm, 				xK_w	 ), spawn "thunar")
  
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "urxvt -e ncmpcpp")
@@ -150,15 +153,15 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-    ++
+   -- ++
  
     --
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    --[((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+     --   | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+      --  , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
  
  
 ------------------------------------------------------------------------
